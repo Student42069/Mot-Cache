e22770e097e08fa06ae1cb34148ffc118c41c127
Leonid Glazyrin <glazyrin.leonid@courrier.uqam.ca>
2022-11-03 13:07:54 -0400

    Search downward

--- motcache.c
+++ motcache.c
@@ -158,6 +158,35 @@ unsigned int check_previous(char *word, char *puzzle, char *unused_letters, unsi
 	return 0;
 }
 
+unsigned int check_down(char *word, char *puzzle, char *unused_letters, unsigned int position) {
+	char found[13] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'};
+	found[0] = puzzle[position];
+	unsigned int used_positions[MAX_WORD_LENGTH];
+	used_positions[0] = position;
+	// printf("%s", found);
+	// printf("%s\n", word);
+	while (strstr(word, found) != NULL) {
+		position = position + 12;
+		used_positions[strlen(found)] = position;
+		char cToStr[2];
+		cToStr[1] = '\0';
+		cToStr[0] = puzzle[position];
+		// printf("%s", cToStr);
+		// if (position % 12 == 0)
+		// 	break;
+		
+		strcat(found, cToStr);
+		// printf("%s : %s : %d : %d\n", found, word, (int)strlen(found), (int)strlen(word));
+		if (strcmp(word, found) == 0) {
+			// puts("Trouve !!");
+			update_unused_letters(unused_letters, used_positions, (int)strlen(found));
+			return 1;
+		}
+	}
+	// puts("Non !");
+	return 0;
+}
+
 unsigned int search_word(char *word, char *puzzle, char *unused_letters) {
 	// printf("%d", (int)strlen(word));
 	unsigned int position;
@@ -165,19 +194,22 @@ unsigned int search_word(char *word, char *puzzle, char *unused_letters) {
 		if (puzzle[position] == word[0]) {
 			// printf("%d : %c\n", position, puzzle[0]);
 			if ((12 - (position % 12)) >= strlen(word))
-				if (check_next(word, puzzle, unused_letters, position)){
-					// printf("Mot trouve horizontal -->> !! : %s\n", word);
+				if (check_next(word, puzzle, unused_letters, position)) {
+					printf("Mot trouve horizontal -->> !! : %s\n", word);
 					return 1;
 				}
 			if (((position % 12) + 1) >= strlen(word))
-				if (check_previous(word, puzzle, unused_letters, position)){
-						// printf("Mot trouve horizontal <<-- !! : %s\n", word);
-						return 1;
-					}
+				if (check_previous(word, puzzle, unused_letters, position)) {
+					printf("Mot trouve horizontal <<-- !! : %s\n", word);
+					return 1;
+				}
 			// if (((position / 12) + 1) >= strlen(word))
 			// 	check_up();
-			// if (12 - ((position / 12) >= strlen(word)))
-			// 	check_down();
+			if (12 - ((position / 12) >= strlen(word)))
+				if (check_down(word, puzzle, unused_letters, position)) {
+					printf("Mot trouve vertical vers le bas !! : %s\n", word);
+					return 1;
+				}
 		}
 	}
 	return 0;
